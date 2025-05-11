9a2eb25b028ed18f0a712b9de65efdd3f83d62f7
TheSpiciestRamen <159206642+TheSpiciestRamen@users.noreply.github.com>
2025-05-11 21:32:37 +1000

    Added detection box for enemy hit detection

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+
+    public float range;
+    public float verticalRange;
+
+    private BoxCollider gunTrigger;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gunTrigger = GetComponent<BoxCollider>();
+        gunTrigger.size = new Vector3(1, verticalRange, range);
+        gunTrigger.center = new Vector3(0, 0, range * 0.5f);
     }
 
     // Update is called once per frame
