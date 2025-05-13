ed4f9e4a8c496a27068a19905a94ecd6b8d860c7
TheSpiciestRamen <159206642+TheSpiciestRamen@users.noreply.github.com>
2025-05-13 11:55:51 +1000

    kept working on enemy hit detection

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 
+    public EnemyManager enemyManagerGun; //setting up an enemy manager component within this document only
+
     private float enemyHealth = 1f;
 
     // Start is called before the first frame update
@@ -16,6 +18,15 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(enemyHealth <= 0)
+        {
+            enemyManagerGun.RemoveEnemy(this);
+            Destroy(gameObject);
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        enemyHealth -= damage;
     }
 }
